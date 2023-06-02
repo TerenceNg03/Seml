e055defe8132b170aa9e8c999dbb3161fe99abcc
TerenceNg03 <stoicism03@gmail.com>
2023-06-02 21:29:19 +0800

    Make output prettier

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,22 @@
 module Main (main) where
 
+import Data.Either (fromRight)
 import Lib
+import Text.Pretty.Simple (CheckColorTty (CheckColorTty), OutputOptions (outputOptionsCompact, outputOptionsCompactParens), defaultOutputOptionsLightBg, pPrintOpt)
 
 main :: IO ()
-main = do
-    print $
-        runParser
-            semlP
-            "(?xml (:version '1.0') (:encoding 'utf-8'))\
-            \    (ksiazka-telefoniczna (:category 'bohaterowie książek')\
-            \      (osoba :charakter 'dobry'\
-            \        (imie Ambroży)\
-            \        (nazwisko Kleks)\
-            \        (telefon 123-456-789))\
-            \      (osoba :charakter 'zły'\
-            \        (imie Alojzy)\
-            \        (nazwisko Bąbel)\
-            \        (telefon)))"
+main =
+    let opts = defaultOutputOptionsLightBg{outputOptionsCompact = True, outputOptionsCompactParens = True}
+     in pPrintOpt CheckColorTty opts . fromRight undefined . snd $
+            runParser
+                semlP
+                "(?xml (:version '1.0') (:encoding 'utf-8'))\
+                \    (ksiazka-telefoniczna (:category 'bohaterowie książek')\
+                \      (osoba (:charakter 'dobry')\
+                \        (imie Ambroży)\
+                \        (nazwisko Kleks)\
+                \        (telefon 123-456-789))\
+                \      (osoba (:charakter 'zły')\
+                \        (imie Alojzy)\
+                \        (nazwisko Bąbel)\
+                \        (telefon)))"
